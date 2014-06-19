c03e002c1230dbea8f4c75c90587c6861a8bfb00
Stas Ostapenko <stas.ostapenko@gmail.com>
2014-06-19 20:32:30 +0300

    example jersey test added

---
+++ JamaExampleWebApp/src/test/java/com/blogspot/ostas/examples/helloworld/spring/SimpleTest.java
@@ -0,0 +1,23 @@
+package com.blogspot.ostas.examples.helloworld.spring;
+
+import org.glassfish.jersey.examples.helloworld.spring.MyWebService;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleTest extends JerseyTest {
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(MyWebService.class);
+    }
+
+    @Test
+    public void test() {
+        final String hello = target("spring-hello").request().get(String.class);
+        assertEquals("Hello World!", hello);
+    }
+}
