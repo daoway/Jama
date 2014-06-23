9b3ffae448f837899e33499248ede7252f87c1a5
Stas Ostapenko <stas.ostapenko@gmail.com>
2014-06-24 00:36:10 +0300

    jak shaving

--- Jama/src/main/java/com/blogspot/ostas/monitoring/preprocess/PointcutClassesCandidate.java
+++
@@ -1,7 +0,0 @@
-package com.blogspot.ostas.monitoring.preprocess;
-
-import java.util.Set;
-
-public interface PointcutClassesCandidate {
-    Set<Class<? extends Object>> getClassList();
-}
--- Jama/src/main/java/com/blogspot/ostas/monitoring/publish/naming/impl/PackageLessMethodNamingStrategy.java
+++ Jama/src/main/java/com/blogspot/ostas/monitoring/publish/naming/impl/PackageLessMethodNamingStrategy.java
@@ -12,31 +12,28 @@ public class PackageLessMethodNamingStrategy implements MethodNamingStrategy {
     @Override
     public String getMethodAsString(Method method) {
         String methodID = cache.get(method);
-        if (methodID == null) {
-            final StringBuilder stringBuffer = new StringBuilder();
-            stringBuffer.append(method.getReturnType().getSimpleName());
-            stringBuffer.append(" ");
-            stringBuffer.append(method.getDeclaringClass().getSimpleName()); //recheck
-            stringBuffer.append("::");
-            stringBuffer.append(method.getName());
-            final Class<?>[] paramTypes = method.getParameterTypes();
-            if (paramTypes.length != 0) {
-                stringBuffer.append("(");
-                for (int j = 0; j < paramTypes.length; j++) {
-                    stringBuffer.append(paramTypes[j].getSimpleName());
-                    if (j != paramTypes.length - 1) {
-                        stringBuffer.append(",");
-                    }
+        if (methodID != null) return methodID;
+        final StringBuilder stringBuffer = new StringBuilder();
+        stringBuffer.append(method.getReturnType().getSimpleName());
+        stringBuffer.append(" ");
+        stringBuffer.append(method.getDeclaringClass().getSimpleName()); //recheck
+        stringBuffer.append("::");
+        stringBuffer.append(method.getName());
+        final Class<?>[] paramTypes = method.getParameterTypes();
+        if (paramTypes.length != 0) {
+            stringBuffer.append("(");
+            for (int j = 0; j < paramTypes.length; j++) {
+                stringBuffer.append(paramTypes[j].getSimpleName());
+                if (j != paramTypes.length - 1) {
+                    stringBuffer.append(",");
                 }
-                stringBuffer.append(")");
-            } else {
-                stringBuffer.append("()");
             }
-            final String id = stringBuffer.toString();
-            cache.put(method, id);
-            return id;
+            stringBuffer.append(")");
         } else {
-            return methodID;
+            stringBuffer.append("()");
         }
+        final String id = stringBuffer.toString();
+        cache.put(method, id);
+        return id;
     }
 }
